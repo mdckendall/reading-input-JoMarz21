6efe1fc1c6c6fd4d28bee9b2565e276118e75382
J0hnny123 <81181721+JoMarz21@users.noreply.github.com>
2023-02-26 22:09:24 +0000

    Added the word the user inputted to the output.

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Enter a word:");
-    
-    //Your code will return the length of the entered String
+    Scanner word = new Scanner(System.in);
+    System.out.println("Enter the word you would like to find the length of: ");
+    String userInput = word.next();
+            int len = userInput.length();
+     System.out.println("Length of the word " + userInput + " is: " + len);
   }
 }
